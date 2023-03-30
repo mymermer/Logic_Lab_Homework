7a6fb80d9df018fbea1f1769f9ac0a16bba5d800
AbdullahSh20 <abdullah.shammout@hotmail.com>
2023-03-30 13:52:03 +0300

    test

--- sim_1/new/modules_test.v
+++ sim_1/new/modules_test.v
@@ -13,25 +13,39 @@ module SR_latch_test();
     SR_latch uut(set,reset,Q,notQ);
     
     initial begin
-        set=0; reset=0; #250;
-        set=1; reset=0; #250;
-        set=0; reset=1; #250;
-        set=1; reset=1; #250;
+        set=1; reset=1; #200;
+        set=0; reset=0; #200;
+        set=1; reset=0; #200;
+        set=0; reset=1; #200;
+        set=1; reset=1; #200;
     end
 endmodule
 
 module enabled_SR_latch_test();
     reg enabled,set,reset;
-    wire Q,notQ;
-    enabled_SR_latch uut(enabled,set,reset,Q,notQ);
+    wire Q,Qnot;
+    enabled_SR_latch uut(enabled,set,reset,Q,Qnot);
     initial begin
-        enabled= 0; set=0; reset=0; #125;
-        enabled= 0; set=0; reset=1; #125;
-        enabled= 0; set=1; reset=0; #125;
-        enabled= 0; set=1; reset=1; #125;
-        enabled= 1; set=0; reset=0; #125;
-        enabled= 1; set=0; reset=1; #125;
-        enabled= 1; set=1; reset=0; #125;
-        enabled= 1; set=1; reset=1; #125;
+        enabled= 0; set=0; reset=0; #150;
+        enabled= 1; set=0; reset=0; #150;
+        enabled= 1; set=0; reset=1; #150;
+        enabled= 1; set=1; reset=0; #150;
+        enabled= 1; set=0; reset=0; #150;
+        enabled= 1; set=1; reset=1; #150;
     end   
+endmodule
+
+
+module JK_flipflop_test();
+reg J,K,clock;
+wire Q,Qnot;
+JK_flipflop uut(J,K,clock,Q,Qnot);
+
+initial begin
+    J=1'b 0; K=1'b 0; #250;
+    J=1'b 0; K=1'b 1; #250;
+    J=1'b 1; K=1'b 0; #250;
+    J=1'b 1; K=1'b 1; #250;
+end
+always #125 clock=~clock;
 endmodule
--- sources_1/new/modules.v
+++ sources_1/new/modules.v
@@ -11,17 +11,17 @@ module nand_module(in1,in2,o);
     assign o = ~(in1 & in2);
 endmodule
 
-module SR_latch(set,reset,Q,notQ);
+module SR_latch(set,reset,Q,Qnot);
     input wire set;
     input wire reset;
     output wire Q;
-    output wire notQ;
+    output wire Qnot;
     
-    nand_module A(set, notQ, Q);
-    nand_module B(reset, Q, notQ);
+    nand_module A(set, Qnot, Q);
+    nand_module B(reset, Q, Qnot);
 endmodule
 
-module enabled_SR_latch(enabled,set,reset,Q,notQ);
+module enabled_SR_latch(enabled,set,reset,Q,Qnot);
     input wire enabled;
     input wire set;
     input wire reset;
@@ -29,9 +29,31 @@ module enabled_SR_latch(enabled,set,reset,Q,notQ);
     wire notset,notreset;
     
     output wire Q;
-    output wire notQ;
+    output wire Qnot;
     
     nand_module E1(enabled,set,notset);
     nand_module E2(enabled,reset,notreset);
-    SR_latch SR1(notset,notreset,Q,notQ);
+
+    SR_latch SR1(notset,notreset,Q,Qnot);
 endmodule
+
+
+module JK_flipflop(input wire J, input wire K, input wire clock, output wire Q, output wire Qnot);
+    wire notOfJQnot, JQnot, snot;
+    wire notOfKQ,KQ,rnot;
+    
+
+    nand_module nand1(J,Qnot,notOfJQnot);
+    nand_module nand2(notOfJQnot,notOfJQnot,JQnot);
+    nand_module nand3(JQnot,clock,snot);
+    
+    nand_module nand4(K,Q,notOfKQ);
+    nand_module nand5(notOfKQ,notOfKQ,KQ);
+    nand_module nand6(KQ,clock,rnot);
+
+    nand_module nand7(snot,Qnot, Q);
+    nand_module nand8(rnot,Q,Qnot);
+
+
+endmodule
+
